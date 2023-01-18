72a06d60e0b14c9bde7836da45e02c2b33291886
rIftimie <thunderray16@gmail.com>
2023-01-18 12:54:00 +0100

    2.8: The Phonebook Step3

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,25 +1,33 @@
 import { useState } from "react";
 
 function App() {
-    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [persons, setPersons] = useState([
+        { name: "Arto Hellas", number: "123 - 234 - 232 - 232" },
+    ]);
     const [newName, setNewName] = useState("");
+    const [newNumber, setNewNumber] = useState(null);
 
     function handleNewName(e) {
         setNewName(e.target.value);
+    }
+    function handleNewNumber(e) {
+        setNewNumber(e.target.value);
     }
 
     function addPerson(e) {
         e.preventDefault();
         const names = persons.map((person) => person.name);
         if (!names.includes(newName)) {
-            setPersons(persons.concat({ name: newName }));
+            setPersons(persons.concat({ name: newName, number: newNumber }));
         } else {
             window.alert(newName + " already exists");
         }
     }
 
     const renderNames = persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+            {person.name} - {person.number}
+        </p>
     ));
 
     return (
@@ -28,11 +36,12 @@ function App() {
             <form onSubmit={(e) => addPerson(e)}>
                 <label>
                     name:
-                    <input
-                        onChange={(e) => handleNewName(e)}
-                        name={"inputName"}
-                        type="text"
-                    />
+                    <input onChange={(e) => handleNewName(e)} type="text" />
+                </label>
+                <br />
+                <label>
+                    number:
+                    <input onChange={(e) => handleNewNumber(e)} type="text" />
                 </label>
                 <label>
                     <button type="submit">add</button>
